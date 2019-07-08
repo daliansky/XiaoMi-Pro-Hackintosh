a4bd9d0afc60b6638a54e51b72c7e75f5e06cd97
Steve <zheng.shi.qi@qq.com>
2019-07-07 23:22:28 -0400

    Update scripts

--- one-key-cpufriend/one-key-cpufriend_cn.sh
+++ one-key-cpufriend/one-key-cpufriend_cn.sh
@@ -34,6 +34,7 @@ function checkPlist() {
   if [[ ! -f "${X86_PLIST}" ]]; then
     # 使用MBP14,1的plist, 如果没有Mac-827FB448E656EC26.plist
     BOARD_ID="Mac-B4831CEBD52A0C4C"
+    X86_PLIST="/System/Library/Extensions/IOPlatformPluginFamily.kext/Contents/PlugIns/X86PlatformPlugin.kext/Contents/Resources/${BOARD_ID}.plist"
   fi
 }
 
