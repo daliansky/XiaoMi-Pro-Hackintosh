2f296452f16ac62fb31c7616447fa4ebd1ea7f28
stevezhengshiqi <zheng.shi.qi@qq.com>
2021-09-08 17:34:19 -0400

    Revert "makefile: Temporarily disable cpplint & cldoc install in VoodooI2C build"
    This reverts commit 056733fb3a4be8e22a5f36ee893851db59866b54.

--- makefile.sh
+++ makefile.sh
@@ -470,17 +470,17 @@ function bKextHelper() {
       fi
       git submodule init -q && git submodule update -q || networkErr "VoodooI2C Satellites"
 
-    # if [[ -z ${GITHUB_ACTIONS+x} ]]; then
+      if [[ -z ${GITHUB_ACTIONS+x} ]]; then
         # Delete Linting & Generate Documentation in Build Phase to avoid installing cpplint & cldoc
         lineNum=$(grep -n "Linting" VoodooI2C/VoodooI2C.xcodeproj/project.pbxproj) && lineNum=${lineNum%%:*}
         /usr/bin/sed -i '' "${lineNum}d" VoodooI2C/VoodooI2C.xcodeproj/project.pbxproj
         lineNum=$(grep -n "Generate Documentation" VoodooI2C/VoodooI2C.xcodeproj/project.pbxproj) && lineNum=${lineNum%%:*}
         /usr/bin/sed -i '' "${lineNum}d" VoodooI2C/VoodooI2C.xcodeproj/project.pbxproj
-    # else
+      else
         # Install cpplint & cldoc when using GitHub Action
-      # pip3 install -q cpplint || exit 1
-      # pip3 install -q git+https://github.com/VoodooI2C/cldoc.git || exit 1
-    # fi
+        pip3 install -q cpplint || exit 1
+        pip3 install -q git+https://github.com/VoodooI2C/cldoc.git || exit 1
+      fi
 
       xcodebuild -workspace "VoodooI2C.xcworkspace" -scheme "VoodooI2C" -derivedDataPath . clean build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO > /dev/null 2>&1 || buildErr "$2"
       cp -R ${PATH_VI2C}*.kext "../" || copyErr
